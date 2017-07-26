a75d7b5acd57b004f0fa59e35a504d1becfc5d7d
RobinScout <robin.gransengblom@gmail.com>
2017-07-26 15:07:22 +0200

    add support for matching properties (#5)
    * add error response event
    * add slot for access denied

--- src/js/vue-guard-component.js
+++ src/js/vue-guard-component.js
@@ -1,5 +1,6 @@
 let guardConfig = {
     path: '/api/v1/route/access',
+    property: 'id',
 }
 
 const guard = {
@@ -15,23 +16,49 @@ const guard = {
             } else {
                 return guardConfig.path;
             }
-        }
+        },
+        postData () {
+            if (this.value !== undefined) {
+                return {
+                    resource: this.resource,
+                    property: this.property,
+                    value: this.value,
+                };
+            } else {
+                return { resource: this.resource };
+            }
+        },
+        noAccessSlot () {
+            if (this.$slots.noAccess !== undefined) {
+                return this.$slots.noAccess;
+            } else if (this.$slots.default !== undefined) {
+                return this.$slots.default.find((slot) => {
+                    return slot.data !== undefined && slot.data.slot === 'noAccess';
+                });
+            }
+
+            return undefined;
+        },
     },
     render (createElement) {
         if (this.show && this.$slots.default !== undefined) {
             return this.$slots.default[0];
+        } else if (this.noAccessSlot !== undefined) {
+            return this.noAccessSlot;
         } else {
             return;
         }
     },
     created () {
-        this.$http.post(this.ajaxUrl, { resource: this.resource }).then(response => {
+        this.$http.post(this.ajaxUrl, this.postData).then(response => {
             if (JSON.parse(response.data)) {
                 this.show = true;
                 this.$emit('guard-accepted');
             } else {
                 this.$emit('guard-denied');
             }
+        }, response => {
+            this.$emit('guard-error');
         });
     },
     props: {
@@ -42,6 +69,14 @@ const guard = {
         resource: {
             type: String,
             required: true,
+        },
+        property: {
+            type: String,
+            required: false,
+            default: guardConfig.property,
+        },
+        value: {
+            required: false,
         }
     }
 }
